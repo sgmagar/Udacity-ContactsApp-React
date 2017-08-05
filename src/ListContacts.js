93c61edefbb926012168bab9352f1c0ab77ccba5
Saurav <sp.gharti@gmail.com>
2017-08-05 12:22:24 +0545

    display contacts and filter

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,13 +1,79 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import escapeRegexExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component{
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+		onDeleteContact: PropTypes.func.isRequired
+	}
+
+	state ={
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({query: query.trim()})
+	}
+
+	clearQuery = () => {
+		this.setState({query: ''})
+	}
 	render() {
+		const { contacts, onDeleteContact } = this.props
+		const { query } = this.state
+		let showingContacts
+		
+		if(query){
+			const match = new RegExp(escapeRegexExp(query), 'i')
+			showingContacts = contacts.filter((contact) => match.test(contact.name))
+		}else{
+			showingContacts = contacts
+		}
+		showingContacts.sort(sortBy('name'))
+
 		return (
-			<ol>
-				
-			</ol>
+			<div className='list-contacts'>
+				<div className='list-contacts-top'>
+					<input 
+						className='search-contacts'
+						type='text'
+						placehoder='Search Contacts'
+						value={query}
+						onChange={(event) => this.updateQuery(event.target.value)}
+					/>
+				</div>
+				{showingContacts.length !== contacts.length &&(
+					<div className='showing-contacts'>
+						<span>Now showing  {showingContacts.length} of {contacts.length}</span>
+						<button onClick={this.clearQuery}>Show All</button>
+					</div>
+				)}
+				<ol>
+					{showingContacts.map((contact) => (
+						<li key={contact.id} className='contact-list-item'>
+							<div className='contact-avatar' style={{
+								backgroundImage: `url(${contact.avatarURL})`
+							}}/>
+							<div className='contact-details'>
+								<p>{contact.name}</p>
+								<p>{contact.email}</p>
+							</div>
+							<button onClick={() => onDeleteContact(contact)} className='contact-remove'>
+								Remove
+							</button>
+						</li>
+					))}
+				</ol>
+			</div>
 		)
 	}
+}
+
+ListContacts.propTypes = {
+	contacts: PropTypes.array.isRequired,
+	onDeleteContact: PropTypes.func.isRequired
 }
 
 export default ListContacts
